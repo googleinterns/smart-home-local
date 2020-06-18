d55bf3970539174abc232136d1f11489df66433a
Colin Daly <cjdaly@google.com>
2020-06-18 15:28:19 -0400

    complete identify flow

--- common/mock-local-home-platform.ts
+++ common/mock-local-home-platform.ts
@@ -11,46 +11,93 @@ interface ScanConfig {
 }
 
 export class UDPScanConfig implements ScanConfig {
-  state: ScanState;gt
-  broadcastaddress: string;
-  listenport: number;
-  broadcastport: number;
-  discoverypacket: string;
+  state: ScanState;
+  broadcastAddress: string;
+  listenPort: number;
+  broadcastPort: number;
+  discoveryPacket: string;
   constructor(
     state: ScanState,
-    broadcastaddress: string,
+    broadcastAddress: string,
     listenport: number,
-    broadcastport: number,
-    discoverypacket: string
+    broadcastPort: number,
+    discoveryPacket: string
   ) {
     this.state = state;
-    this.broadcastaddress = broadcastaddress;
-    this.listenport = listenport;
-    this.discoverypacket = discoverypacket;
+    this.broadcastAddress = broadcastAddress;
+    this.broadcastPort = broadcastPort;
+    this.listenPort = listenport;
+    this.discoveryPacket = discoveryPacket;
   }
 }
 
 // TODO add other radio support
-export class MockLocalHomePlatform implements MockUDPListener{
-  private udpdcanConfigs: UDPScanConfig[];
+export class MockLocalHomePlatform implements MockUDPListener {
+  private udpScanConfigs: UDPScanConfig[] = [];
   private network: MockNetwork;
 
-  constructor(network: MockNetwork) {
+  constructor(network: MockNetwork, udpScanConfigs: UDPScanConfig[]) {
     this.network = network;
-    this.udpdcanConfigs = [];
+    this.udpScanConfigs = udpScanConfigs;
+    this.setupUDP();
   }
 
-  // Establish fulfillment path using app code 
+  // UDP Scan Functionaity
+
+  private setupUDP() {
+    this.udpScanConfigs.forEach((udpScanConfig) => {
+      this.network.registerUDPListener(
+        this,
+        udpScanConfig.listenPort,
+        udpScanConfig.broadcastAddress
+      );
+    });
+  }
+
+  private sendUDPBroadcast(scanConfig: UDPScanConfig) {
+    const packetBuffer = Buffer.from(scanConfig.discoveryPacket, 'hex');
+    this.network.sendUDPMessage(
+      packetBuffer,
+      scanConfig.broadcastPort,
+      scanConfig.broadcastAddress,
+      scanConfig.listenPort,
+      scanConfig.broadcastAddress
+    );
+  }
+
+  // Establish fulfillment path using app code
   onUDPMessage(msg: Buffer, rinfo: Object): void {
-
+    console.log('received discovery payload:', msg, 'from:', rinfo);
+    const identifyRequest: string = JSON.stringify({
+      requestId: 'request-id',
+      inputs: [
+        {
+          intent: 'action.devices.IDENTIFY',
+          payload: {
+            device: {
+              radioTypes: [],
+              udpScanData: msg.toString('hex'),
+            },
+            structureData: {},
+            params: {},
+          },
+        },
+      ],
+      devices: [],
+      rinfo,
+    });
+    const identifyBuffer: Buffer = Buffer.from(identifyRequest, 'utf-8');
+    // TODO send this to app.identifyHandler
   }
 
-  addUDPScanConfig(scanConfig: UDPScanConfig) {
-    this.udpdcanConfigs.push(scanConfig);
+  public addUDPScanConfig(scanConfig: UDPScanConfig) {
+    this.udpScanConfigs.push(scanConfig);
   }
 
-  triggerScan() {
+  public triggerScan() {
     // First, UDP
-    // Send a discovery message out
+    this.udpScanConfigs.forEach((udpScanConfig) => {
+      this.sendUDPBroadcast(udpScanConfig);
+    });
   }
 }
