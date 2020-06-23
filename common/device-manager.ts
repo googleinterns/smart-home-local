4467264328ca8f4713e54f43983833e03a6e0c30
Colin Daly <cjdaly@google.com>
2020-06-23 19:37:21 -0400

    PR tweaks, add some dotfiles

--- common/device-manager.ts
+++ common/device-manager.ts
@@ -11,31 +11,14 @@ class DeviceManager {
   error: boolean;
   public commands = new Array<smarthome.DataFlow.TcpRequestData>();
 
-  /**
-   * `send` is called by app when it needs to communicate with a device.
-   * Depending upon the protocol used by the device, the app constructs a
-   * [[DataFlow.CommandRequest]] object and passes it as an argument.
-   * Returns a promise that resolves to [[DataFlow.CommandSuccess]]. Response
-   * may return data, if it was a read request.
-   * @param command  Command to communicate with the device.
-   * @return  Promise that resolves to [[DataFlow.CommandSuccess]]
-   **/
-
-  public send(command: smarthome.DataFlow.TcpRequestData): Promise<any> {
+  public send(
+    command: smarthome.DataFlow.TcpRequestData
+  ): Promise<smarthome.DataFlow.CommandSuccess> {
     if (this.error) {
       return Promise.reject(this.error);
     }
     this.commands.push(command);
-    return Promise.resolve(this.deviceId);
+    //TODO(cjdaly) build a proper CommandSuccess
+    return Promise.resolve(new smarthome.DataFlow.HttpRequestData());
   }
-}
-/**
- * Stub function
- **/
-
-export function deviceManagerStub(deviceId: string, error?: any) {
-  const deviceManager = new DeviceManager();
-  deviceManager.deviceId = deviceId;
-  deviceManager.error = error;
-  return deviceManager;
-}
+};
