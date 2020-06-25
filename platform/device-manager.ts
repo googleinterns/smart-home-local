89c56814c9e54d749edbed8076834338eebb1d24
Colin Daly <cjdaly@google.com>
2020-06-25 18:55:17 -0400

    remove incomplete code, improve README

--- platform/device-manager.ts
+++ platform/device-manager.ts
@@ -2,25 +2,15 @@
  * DeviceManager Stub
  * TODO(cjdaly) integrate with Mock Local Home Platform and Mock Network
  **/
-/// <reference types="@google/local-home-sdk" />
 
-import { DeviceManager } from "@google/local-home-sdk";
+// <reference types="@google/local-home-sdk" />
+import * as smarthome from '@google/local-home-sdk';
 
-export class DeviceManagerStub implements DeviceManager{
-
-  markPending(request: smarthome.IntentRequest): Promise<void> {
-    //TODO(cjdaly) implementation
-    throw new Error("Method not implemented.");
-  }
-
-  getProxyInfo(id: string): smarthome.ProxyInfo {
-    //TODO(cjdaly) implementation
-    throw new Error("Method not implemented.");
-  }
-
+class DeviceManager {
   deviceId: string;
   error: boolean;
-  commands: smarthome.DataFlow.TcpRequestData[] = [];
+  public commands = new Array<smarthome.DataFlow.TcpRequestData>();
+
   public send(
     command: smarthome.DataFlow.TcpRequestData
   ): Promise<smarthome.DataFlow.CommandSuccess> {
