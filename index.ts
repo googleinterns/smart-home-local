8c7ecb128f7da4cc479b6a396a53ee465ca04f1a
Colin Daly <cjdaly@google.com>
2020-06-29 19:21:28 -0400

    remove unnecessary imports

--- index.ts
+++ index.ts
@@ -1,14 +1,2 @@
-import {
-  UDPDevice,
-  MockNetwork,
-  RemoteAddressInfo,
-} from './platform/mock-radio';
-
-import {
-  MockLocalHomePlatform,
-  UDPScanConfig,
-  ScanState,
-} from './platform/mock-local-home-platform';
-
 export * from './platform/mock-radio';
 export * from './platform/mock-local-home-platform';
