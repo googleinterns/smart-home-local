f391c4b85ad14960894aac5888226b350e811836
Colin Daly <cjdaly@google.com>
2020-08-21 15:25:41 +0000

    change scan results buffer format (#59)
    fix merge conflicts

--- cli/src/platform-worker.ts
+++ cli/src/platform-worker.ts
@@ -64,7 +64,7 @@ export class PlatformWorker {
           // Trigger the identifyHandler with scan results.
           await this.mockLocalHomePlatform.triggerIdentify(
             scanMessage.requestId,
-            scanRemoteInfo.buffer,
+            Buffer.from(scanRemoteInfo.scanData, 'hex'),
             scanMessage.deviceId
           );
           // Save association between deviceId and local IP address.
